16e94a1ef91e71e6994c566c78518b6cc8610e88
Sh0tgunM3ssiah <31352611+Sh0tgunM3ssiah@users.noreply.github.com>
2024-04-08 15:14:24 -0500

    more prod
    more prod

--- server/index.js
+++ server/index.js
@@ -274,7 +274,7 @@ app.use("/events", eventsRoutes);
 app.use("/services", serviceRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
