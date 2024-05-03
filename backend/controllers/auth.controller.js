aae79d5256f5a9c3178118b2666593a879d5fd33
Sh0tgunM3ssiah <31352611+Sh0tgunM3ssiah@users.noreply.github.com>
2024-05-03 13:37:14 -0500

    api attempt 100

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -46,12 +46,6 @@ export const signup = async (req, res) => {
 			const token = jwt.sign( newUser._id, process.env.JWT_SECRET, {
 				expiresIn: "15d",
 			});
-			res.cookie("jwt", token, {
-				maxAge: 15 * 24 * 60 * 60 * 1000, //MS
-				httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-				sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-				secure: process.env.NODE_ENV !== "development",
-			});
 			await newUser.save();
 
 			res.status(201).json({
@@ -88,12 +82,6 @@ export const login = async (req, res) => {
 
 		const token = jwt.sign( user._id, process.env.JWT_SECRET, {
 			expiresIn: "15d",
-		});
-		res.cookie("jwt", token, {
-			maxAge: 15 * 24 * 60 * 60 * 1000, //MS
-			httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-			sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-			secure: process.env.NODE_ENV !== "development",
 		});
 
 		res.status(200).json({
