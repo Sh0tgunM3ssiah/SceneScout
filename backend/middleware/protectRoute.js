b903e917aba19d85848da3ce0e4611c975fde027
Sh0tgunM3ssiah <31352611+Sh0tgunM3ssiah@users.noreply.github.com>
2024-05-04 12:11:58 -0500

    Update protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,7 +2,8 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
-
+    console.log(req);
+    console.log(req.cookies);
 	try {
 		const token = req.cookies.jwt;
 		if (!token) {
