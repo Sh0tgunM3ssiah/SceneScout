f9332375e4f831a3fa9140d1dfad69a6f8cef06c
Sh0tgunM3ssiah <31352611+Sh0tgunM3ssiah@users.noreply.github.com>
2024-05-04 13:13:00 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,22 @@ cloudinary.config({
 });
 
 const app = express();
+app.use(cookieParser());
+
+app.get('/setBackendCookie', (req, res) => {
+	const jwtCookie = req.cookies.jwt; // Grab the "jwt" cookie from the request
+  
+	// Set the "jwt" cookie on the backend domain
+	res.cookie('jwt', jwtCookie, {
+	  domain: '.scenescout-backend.render.com',
+	  httpOnly: true,
+	  sameSite: 'none',
+	  secure: true
+	});
+  
+	res.send('Backend JWT cookie set successfully');
+  });
+
 
 app.use(cors());
 const PORT = process.env.PORT || 8080;
@@ -29,8 +45,6 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-
-app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
