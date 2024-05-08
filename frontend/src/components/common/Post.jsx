390a55c2c15606ae36dd4b8996e6f1d2a175677d
Sh0tgunM3ssiah <31352611+Sh0tgunM3ssiah@users.noreply.github.com>
2024-05-07 20:17:57 -0500

    added comment modal back

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -2,7 +2,7 @@ import { FaRegComment } from "react-icons/fa";
 import { BiRepost } from "react-icons/bi";
 import { FaRegHeart } from "react-icons/fa";
 import { FaRegBookmark, FaArrowTurnDown, FaLocationDot } from "react-icons/fa6";
-import { FaTrash } from "react-icons/fa";
+import { FaTrash, } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -231,6 +231,55 @@ const Post = ({ post }) => {
                                     {post.comments.length}
                                 </span>
                             </div>
+							<dialog id={`comments_modal${post._id}`} className='modal border-none outline-none'>
+								<div className='modal-box rounded border border-gray-600'>
+									<h3 className='font-bold text-lg mb-4'>COMMENTS</h3>
+									<div className='flex flex-col gap-3 max-h-60 overflow-auto'>
+										{post.comments.length === 0 && (
+											<p className='text-sm text-slate-500'>
+												No comments yet 🤔 Be the first one 😉
+											</p>
+										)}
+										{post.comments.map((comment) => (
+											<div key={comment._id} className='flex gap-2 items-start'>
+												<div className='avatar'>
+													<div className='w-8 rounded-full'>
+														<img
+															src={comment.user.profileImg || "/avatar-placeholder.png"}
+														/>
+													</div>
+												</div>
+												<div className='flex flex-col'>
+													<div className='flex items-center gap-1'>
+														<span className='font-bold'>{comment.user.fullName}</span>
+														<span className='text-gray-700 text-sm'>
+															@{comment.user.username}
+														</span>
+													</div>
+													<div className='text-sm'>{comment.text}</div>
+												</div>
+											</div>
+										))}
+									</div>
+									<form
+										className='flex gap-2 items-center mt-4 border-t border-gray-600 pt-2'
+										onSubmit={handlePostComment}
+									>
+										<textarea
+											className='textarea w-full p-1 rounded text-md resize-none border focus:outline-none  border-gray-800'
+											placeholder='Add a comment...'
+											value={comment}
+											onChange={(e) => setComment(e.target.value)}
+										/>
+										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
+											{isCommenting ? <LoadingSpinner size='md' /> : "Post"}
+										</button>
+									</form>
+								</div>
+								<form method='dialog' className='modal-backdrop'>
+									<button className='outline-none'>close</button>
+								</form>
+							</dialog>
 								<div className='flex gap-1 items-center group cursor-pointer'>
 									<BiRepost className='w-6 h-6  text-slate-500 group-hover:text-green-500' onClick={handleSharePost} />
 									<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
